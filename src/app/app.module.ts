c8f6fda338799de6e048cb0e986ab6a2194d43ff
Marco Sangalli <msangalli@sapient.com>
2019-04-23 20:35:11 +0200

    exporting module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestFsmComponent } from './test-fsm/test-fsm.component';
 import { TestCommentComponent } from './test-comment/test-comment.component';
+import { AngularXstateConnectModule } from 'angular-xstate-connect';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { TestCommentComponent } from './test-comment/test-comment.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularXstateConnectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
