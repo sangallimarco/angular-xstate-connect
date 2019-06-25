a5c427f4a1c54aaa0b757d4c821e6d833417f420
Marco Sangalli <sangalli.marco@gmail.com>
2019-06-25 15:54:46 +0200

    adding experimental decorator

--- projects/angular-xstate-connect/src/lib/angular-xstate-connect.decorator.ts
+++ projects/angular-xstate-connect/src/lib/angular-xstate-connect.decorator.ts
@@ -1,12 +1,12 @@
 
 import { AngularXstateConnectService, StateMachineData } from './angular-xstate-connect.service';
-import { EventObject, StateSchema, DefaultContext, MachineConfig, MachineOptions } from 'xstate';
+import { EventObject, StateSchema, DefaultContext, MachineConfig } from 'xstate';
 import { Injector } from '@angular/core';
 
 
 export interface AngularXstateConnectProps<TStateSchema, TContext, TEvent extends EventObject = EventObject> {
     state: StateMachineData<TContext, TStateSchema> | undefined;
-    stateMachine: AngularXstateConnectService<TStateSchema, TContext, TEvent>
+    stateMachine: AngularXstateConnectService<TStateSchema, TContext, TEvent>,
 }
 
 export function AngularXstateConnect<TStateSchema extends StateSchema, TContext = DefaultContext, TEvent extends EventObject = EventObject>(
@@ -16,32 +16,31 @@ export function AngularXstateConnect<TStateSchema extends StateSchema, TContext 
     }
 ): ClassDecorator {
 
-    type serviceType = AngularXstateConnectService<TStateSchema, TContext, TEvent>;
-
     return function (constructor: any) {
         const { machine, initialContext } = config;
-        const injector = Injector.create({ providers: [{ provide: AngularXstateConnectService, deps: [] }] });
-        const stateMachine: serviceType = injector.get(AngularXstateConnectService) as any;
-        stateMachine.setConfig(machine, initialContext);
-
-        // create state variable
-        constructor.prototype.state = initialContext;
-        constructor.prototype.stateMachine = stateMachine;
+        let stateMachine: AngularXstateConnectService<TStateSchema, TContext, TEvent>;
 
         const ngOnInit = constructor.prototype.ngOnInit;
-
         constructor.prototype.ngOnInit = function (...args) {
+            const injector = Injector.create({ providers: [{ provide: AngularXstateConnectService, deps: [] }] });
+            stateMachine = injector.get(AngularXstateConnectService) as any;
+
+            stateMachine.setConfig(machine, initialContext);
             stateMachine.subscribe((state) => {
                 this.state = state;
             });
+
+            this.stateMachine = stateMachine;
+
             ngOnInit && ngOnInit.apply(this, args);
+
             // configure if not set up
             stateMachine.init({});
         }
 
         const ngOnDestroy = constructor.prototype.ngOnDestroy;
         constructor.prototype.ngOnDestroy = function (...args) {
-            stateMachine.destroy();
+            constructor.stateMachine.destroy();
             ngOnDestroy && ngOnDestroy.apply(this, args);
         }
 
